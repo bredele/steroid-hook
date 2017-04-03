9d45686213da10cdc0ffc7213275b052a94dcc61
bredele <olivier.wietrich@gmail.com>
2017-04-03 16:57:15 -0600

    created structure to compile steroid file

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-require.extensions['.html'] = function() {
-  console.log('hello')
+
+/**
+ * Dependencies
+ */
+
+const read = require('fs').readFileSync
+
+require.extensions['.html'] = function(mod, filename) {
+  const file = read(filename, {encoding: 'utf-8'})
+  return mod._compile(compile(file), filename)
+}
+
+
+/**
+ * Compile file string.
+ *
+ * @param {String} file
+ * @return {String}
+ * @api private
+ */
+
+function compile(file) {
+  return file
 }
