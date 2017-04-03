5864b6b0b148179dbeb59ff788589041e1944b55
bredele <olivier.wietrich@gmail.com>
2017-04-03 16:37:04 -0600

    concat steroid stream to get the result as string

--- test/steroid-hook.js
+++ test/steroid-hook.js
@@ -5,3 +5,19 @@
 const test = require('tape')
 const html = require('steroid')
 const hook = require('..')
+
+
+test('define custom tags using uppercase', assert => {
+  assert.plan(1)
+  
+})
+
+/**
+ * Hello component.
+ *
+ * @api private
+ */
+
+function Hello(params) {
+  return <button class="${params.style}">hello</button>
+}
